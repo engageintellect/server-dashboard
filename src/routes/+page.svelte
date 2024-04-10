1347656ea6cc3e00aab1a3e50af91897906019d1
engageintellect <jc9361@gmail.com>
2024-04-10 00:06:31 -0700

    init WebSockets

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import { onMount, onDestroy } from 'svelte';
+	import { fade } from 'svelte/transition';
+
+	let ws: WebSocket;
 
 	type EndpointKey =
 		| 'hostname'
@@ -13,6 +16,7 @@
 		| 'networkLatency'
 		| 'networkPorts'
 		| 'runningServices';
+
 	const endpoints: Record<EndpointKey, string> = {
 		hostname: '/api/hostname',
 		os: '/api/os',
@@ -52,128 +56,203 @@
 
 	onMount(() => {
 		fetchDataForAllKeys(); // Initial fetch
-		const interval = setInterval(fetchDataForAllKeys, 5000); // Fetch every 5 seconds
-		return () => clearInterval(interval); // Cleanup on component destroy
+
+		// Establish WebSocket connection after 5 seconds
+		setTimeout(() => {
+			ws = new WebSocket(`ws://45.56.88.245:6767/api/ws`);
+			ws.onmessage = (event) => {
+				const newData = JSON.parse(event.data);
+				Object.keys(newData).forEach((key) => {
+					data[key as keyof typeof data] = newData[key as keyof typeof newData];
+				});
+			};
+
+			ws.onerror = (error) => {
+				console.error('WebSocket error:', error);
+			};
+
+			ws.onclose = () => {
+				console.log('WebSocket connection closed');
+			};
+		}, 5000); // 5-second delay
+	});
+
+	onDestroy(() => {
+		if (ws) {
+			ws.close();
+		}
 	});
 </script>
 
-<div class="mx-auto max-w-5xl">
-	<div class="flex w-full flex-col gap-2 overflow-hidden p-2 sm:p-5">
-		<div class="flex w-full flex-row gap-2 sm:flex-row">
-			<div class="card flex-1 bg-base-300">
-				<div class="card-body h-full p-5">
-					<div>Host:</div>
-					<div class="flex-1 text-3xl font-extrabold">
-						{data.hostname}
-					</div>
-				</div>
-			</div>
-
-			<div class="card flex-1 bg-base-300">
-				<div class="card-body h-full p-5">
-					<div>OS:</div>
-					<div class="flex-1 text-sm font-extrabold sm:text-2xl">
-						{data.os}
-					</div>
-				</div>
-			</div>
-
-			<div class="card flex-1 bg-base-300">
-				<div class="card-body h-full p-5">
-					<div>Uptime:</div>
-					<div class="flex-1 text-sm font-extrabold sm:text-2xl">
-						{data.uptime}
-					</div>
-				</div>
+{#if data.hostname === null}
+	<div class="mx-auto min-h-screen max-w-5xl p-2 sm:p-5">
+		<div role="alert" class="alert shadow-lg">
+			<div class="loading loading-spinner loading-md"></div>
+			<div>
+				<h3 class="text-2xl font-bold">Loading Server Data...</h3>
+				<div class="text-xs">Establishing WebSocket connection to server.</div>
 			</div>
 		</div>
-
-		<div class="flex flex-col gap-2 sm:flex-row">
-			<div class="flex w-full gap-2">
-				<div class="card w-full flex-1 bg-base-300">
+	</div>
+{:else}
+	<div class="mx-auto max-w-5xl">
+		<div class="flex w-full flex-col gap-2 overflow-hidden p-2 sm:p-5">
+			<div class="flex w-full flex-row gap-2 sm:flex-row">
+				<div class="card flex-1 bg-base-300">
 					<div class="card-body h-full p-5">
-						<div>Memory Used:</div>
+						<div>Host:</div>
 						<div class="flex-1 text-3xl font-extrabold">
-							{data.memoryUsed}
+							{#if data.hostname === null}
+								<div class="loading loading-spinner loading-md"></div>
+							{:else}
+								{data.hostname}
+							{/if}
 						</div>
 					</div>
 				</div>
 
-				<div class="card w-full flex-1 bg-base-300">
+				<div class="card flex-1 bg-base-300">
 					<div class="card-body h-full p-5">
-						<div>Memory Available:</div>
-						<div class="flex-1 text-3xl font-extrabold">
-							{data.memoryAvailable}
+						<div>OS:</div>
+						<div class="flex-1 text-sm font-extrabold sm:text-2xl">
+							{#if data.os === null}
+								<div class="loading loading-spinner loading-md"></div>
+							{:else}
+								{data.os}
+							{/if}
+						</div>
+					</div>
+				</div>
+
+				<div class="card flex-1 bg-base-300">
+					<div class="card-body h-full p-5">
+						<div>Uptime:</div>
+						<div class="flex-1 text-sm font-extrabold sm:text-2xl">
+							{#if data.uptime === null}
+								<div class="loading loading-spinner loading-md"></div>
+							{:else}
+								{data.uptime}
+							{/if}
 						</div>
 					</div>
 				</div>
 			</div>
 
-			<div class="flex w-full gap-2">
-				<div class="card w-full flex-1 bg-base-300">
-					<div class="card-body h-full p-5">
-						<div>CPU Usage:</div>
-						<div class="flex-1 text-3xl font-extrabold">
-							{data.cpuUsage}
+			<div class="flex flex-col gap-2 sm:flex-row">
+				<div class="flex w-full gap-2">
+					<div class="card w-full flex-1 bg-base-300">
+						<div class="card-body h-full p-5">
+							<div>Memory Used:</div>
+							<div class="flex-1 text-3xl font-extrabold">
+								{#if data.memoryUsed === null}
+									<div class="loading loading-spinner loading-md"></div>
+								{:else}
+									{data.memoryUsed}
+								{/if}
+							</div>
+						</div>
+					</div>
+
+					<div class="card w-full flex-1 bg-base-300">
+						<div class="card-body h-full p-5">
+							<div>Memory Available:</div>
+							<div class="flex-1 text-3xl font-extrabold">
+								{#if data.memoryAvailable === null}
+									<div class="loading loading-spinner loading-md"></div>
+								{:else}
+									{data.memoryAvailable}
+								{/if}
+							</div>
 						</div>
 					</div>
 				</div>
 
-				<div class="card w-full flex-1 bg-base-300">
-					<div class="card-body h-full p-5">
-						<div>Disk Usage:</div>
-						<div class="flex-1 text-3xl font-extrabold">
-							{data.diskUsage}
+				<div class="flex w-full gap-2">
+					<div class="card w-full flex-1 bg-base-300">
+						<div class="card-body h-full p-5">
+							<div>CPU Usage:</div>
+							<div class="flex-1 text-3xl font-extrabold">
+								{#if data.cpuUsage === null}
+									<div class="loading loading-spinner loading-md"></div>
+								{:else}
+									{data.cpuUsage}
+								{/if}
+							</div>
+						</div>
+					</div>
+
+					<div class="card w-full flex-1 bg-base-300">
+						<div class="card-body h-full p-5">
+							<div>Disk Usage:</div>
+							<div class="flex-1 text-3xl font-extrabold">
+								{#if data.diskUsage === null}
+									<div class="loading loading-spinner loading-md"></div>
+								{:else}
+									{data.diskUsage}
+								{/if}
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
 
-		<div class="flex flex-col gap-2 sm:flex-row">
+			<div class="flex flex-col gap-2 sm:flex-row">
+				<div class="card flex-1 bg-base-300">
+					<div class="card-body h-full p-5">
+						<div>Network Usage:</div>
+						<div class="flex-1 text-3xl font-extrabold">
+							{#if data.networkUsage === null}
+								<div class="loading loading-spinner loading-md"></div>
+							{:else}
+								{data.networkUsage}
+							{/if}
+						</div>
+					</div>
+				</div>
+
+				<div class="card flex-1 bg-base-300">
+					<div class="card-body h-full p-5">
+						<div>Network Latency:</div>
+						<div class="flex-1 text-3xl font-extrabold">
+							{#if data.networkLatency === null}
+								<div class="loading loading-spinner loading-md"></div>
+							{:else}
+								{data.networkLatency}
+							{/if}
+						</div>
+					</div>
+				</div>
+
+				<div class="card flex-1 bg-base-300">
+					<div class="card-body h-full p-5">
+						<div>Open Ports:</div>
+						{#if data.networkPorts === null}
+							<div class="loading loading-spinner loading-md"></div>
+						{:else}
+							<div class="flex flex-1 flex-col">
+								{#each data.networkPorts as port}
+									<div class="font-extrabold sm:text-xl">{port}</div>
+								{/each}
+							</div>
+						{/if}
+					</div>
+				</div>
+			</div>
+
 			<div class="card flex-1 bg-base-300">
 				<div class="card-body h-full p-5">
-					<div>Network Usage:</div>
-					<div class="flex-1 text-3xl font-extrabold">
-						{data.networkUsage}
-					</div>
-				</div>
-			</div>
-
-			<div class="card flex-1 bg-base-300">
-				<div class="card-body h-full p-5">
-					<div>Network Latency:</div>
-					<div class="flex-1 text-3xl font-extrabold">
-						{data.networkLatency}
-					</div>
-				</div>
-			</div>
-
-			<div class="card flex-1 bg-base-300">
-				<div class="card-body h-full p-5">
-					<div>Open Ports:</div>
-					{#if data.networkPorts}
-						<div class="flex flex-1 flex-col">
-							{#each data.networkPorts as port}
-								<div class="font-extrabold sm:text-xl">{port}</div>
+					<div>Running Services:</div>
+					{#if data.runningServices == null}
+						<div class="loading loading-spinner loading-md"></div>
+					{:else}
+						<div class="grid grid-cols-1 sm:grid-cols-2">
+							{#each data.runningServices as service}
+								<div class="text-lg font-thin">{service}</div>
 							{/each}
 						</div>
 					{/if}
 				</div>
 			</div>
 		</div>
-
-		<div class="card flex-1 bg-base-300">
-			<div class="card-body h-full p-5">
-				<div>Running Services:</div>
-				{#if data.runningServices}
-					<div class="grid grid-cols-1 sm:grid-cols-2">
-						{#each data.runningServices as service}
-							<div class="text-lg font-thin">{service}</div>
-						{/each}
-					</div>
-				{/if}
-			</div>
-		</div>
 	</div>
-</div>
+{/if}
