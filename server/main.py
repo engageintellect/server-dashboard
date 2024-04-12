c5e017372ff3268cada21e6636dba6f92608255b
engageintellect <jc9361@gmail.com>
2024-04-12 02:45:26 -0700

    init

--- server/main.py
+++ server/main.py
@@ -109,7 +109,6 @@ def get_open_ports():
 
 @app.get("/api/services/running")
 def get_running_services():
-    print('HELLLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO')
     command = "systemctl list-units --type=service --state=running | grep '\.service' | awk '{print $1}'"
     output = subprocess.getoutput(command)
     services = output.split('\n')
