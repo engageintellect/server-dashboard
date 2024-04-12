69a61eae216ac411acd03d11f15b5fccb37fbece
engageintellect <jc9361@gmail.com>
2024-04-11 23:40:34 -0700

    init

--- server/main.py
+++ server/main.py
@@ -72,8 +72,11 @@ def get_upgradable_packages():
 
 @app.get("/api/network/usage")
 def get_network_usage(interface="eth0"):
-    command = f"sar -n DEV 1 1 | awk '/Average: {interface}/{{ printf(\"RX: %.2f KB/s, TX: %.2f KB/s\\n\", $5, $6) }}'"
-    return subprocess.getoutput(command)
+    # command = f"sar -n DEV 1 1 | awk '/Average: {interface}/{{ printf(\"RX: %.2f KB/s, TX: %.2f KB/s\\n\", $5, $6) }}'"
+	received = f"ifconfig eth0 | grep 'RX packets' | awk '{print $5/1024/1024 " MB received"}'"
+	sent = f"ifconfig eth0 | grep 'TX packets' | awk '{print $5/1024/1024 " MB sent"}'"
+
+    return subprocess.getoutput(received, sent)
 
 
 @app.get("/api/network/latency")
