56556a50bd746c5fa2e65180b58275725d3d20da
engageintellect <jc9361@gmail.com>
2024-04-16 18:20:43 -0700

    init

--- server/main.py
+++ server/main.py
@@ -75,6 +75,7 @@ def get_updatable_packages():
 
 
 
+
     output = subprocess.getoutput(command)
     # upgradable_packages = [line for line in output.split(
         # '\n') if 'upgradable from' in line]
